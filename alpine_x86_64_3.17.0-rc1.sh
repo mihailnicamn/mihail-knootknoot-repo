11d779921f93e874c8748df0ff9df61d21fc338a
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2023-01-09 03:12:26 +0200

    Update alpine_x86_64_3.17.0-rc1.sh

--- alpine_x86_64_3.17.0-rc1.sh
+++ alpine_x86_64_3.17.0-rc1.sh
@@ -1,1 +1,2 @@
-RUN echo "alpine setup"
+RUN curl -O https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz
+RUN tar -xzf alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz -C /container
