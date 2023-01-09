18284be70125d94d86bfa8e9fc59ea31d146aa7a
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2023-01-09 03:23:49 +0200

    Update alpine_x86_64_3.17.0-rc1.sh

--- alpine_x86_64_3.17.0-rc1.sh
+++ alpine_x86_64_3.17.0-rc1.sh
@@ -1,4 +1,4 @@
-RUN containerName = shasum KnootFile
+RUN containerName = ($(shasum ./KnootFile))
 RUN mkdir ./container/$containerName
 RUN curl -O https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz
 RUN tar -xzvf alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz -C ./container/$containerName
