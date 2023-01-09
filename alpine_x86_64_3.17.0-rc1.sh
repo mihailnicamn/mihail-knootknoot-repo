0599c69b9e8eef568ae8e30fbe99ff6ecd3251c0
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2023-01-09 21:22:41 +0200

    Update alpine_x86_64_3.17.0-rc1.sh

--- alpine_x86_64_3.17.0-rc1.sh
+++ alpine_x86_64_3.17.0-rc1.sh
@@ -1,9 +1,5 @@
 
-RUN containerName=$(shasum "./KnootFile" | cut -d ' ' -f 1); \
-    	mkdir -p ./container/$containerName; \ 
-    	echo $containerName; \ 
-    	ls -l ./container/$containerName; \ 
-    	curl -O https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz; \ 
-    	tar -xzvf alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz -C ./container/$containerName; \ 
+RUN curl -O https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz; \ 
+    	tar -xzvf alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz -C ./container/$KNOOT_CONTAINER; \ 
     	rm ./alpine-minirootfs-3.17.0_rc1-x86_64.tar.gz; \
 	
